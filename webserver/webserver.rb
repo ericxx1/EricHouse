4e8bac799bbc4bfce0abfea6c0b9458ae5473cb6
ericxx1 <eric-szabo@hotmail.com>
2013-02-23 11:24:17 -0500

    Almost webserver support

--- webserver/webserver.rb
+++ webserver/webserver.rb
@@ -1,19 +1,24 @@
 require 'socket'
 webserver = TCPServer.new('127.0.0.1', 1113)
 while (session = webserver.accept)
-   session.print "HTTP/1.1 200/OK\r\nContent-type:text/html\r\n\r\n"
    request = session.gets
+   if(request =~ /www./)
+   trimmedrequest = request.gsub(/GET\ \//, '').gsub(/\ HTTP.*/, '').gsub('www', '')
+   session.puts "Web Servers are only half working atm!"
+   else
    trimmedrequest = request.gsub(/GET\ \//, '').gsub(/\ HTTP.*/, '')
-   filename = trimmedrequest.chomp
-   if filename == ""
-      filename = "index.html"
+   session.puts "Web Servers are only half working atm!"
    end
-   begin
-      displayfile = File.open(filename, 'r')
-      content = displayfile.read()
-      session.print content
-   rescue Errno::ENOENT
-      session.print "File not found"
-   end
+   #filename = trimmedrequest.chomp
+   #if filename == ""
+   #   filename = "index.html"
+   #end
+   #begin
+   #   displayfile = File.open(filename, 'r')
+   #   content = displayfile.read()
+   #   session.print content
+   #rescue Errno::ENOENT
+   #   session.print "File not found"
+   #end
    session.close
 end
